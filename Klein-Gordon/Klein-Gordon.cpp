0182398593d2a2b6bcaf0c0daf0ec9eb6ec0823d
campioru <campioru@tcd.ie>
2025-08-04 15:55:12 +0200

    class functions
    Re-organising MCMC methods as class functions. Adding and removing some statistical functions.

--- Klein-Gordon/Klein-Gordon.cpp
+++ Klein-Gordon/Klein-Gordon.cpp
@@ -5,261 +5,182 @@
 #include <random>
 using namespace std;
 
-auto r = mt19937 {123};
+mt19937 r(1234);
+uniform_real_distribution<double> standard_uniform(0., 1.);
+normal_distribution<double> standard_normal(0., 1.);
 
 class Field
 {
   private:
     int d_, L_total_;
     vector<int> L_;
-    double am_, κ_;
-    vector<double> data_;
+    double m_;
+    vector<double> φ_;
+    vector<vector<int>> nei_;
+
     int index_(const vector<int>& coord) const
     {
-      int index = coord[0], Π = L_[0];
-      for (int i = 1; i < d_; i ++)
+      int index = coord.front(), Π = L_.front();
+      for (int d = 1; d < d_; d ++)
       {
-        index += coord[i] * Π;
-        Π *= L_[i];
+        index += coord[d] * Π;
+        Π *= L_[d];
       }
       return index;
     }
+
+    double gamma_(const int& x) const
+    {
+      double γ = 0.;
+      for (int d = 0; d < d_; d ++) γ += φ_[nei_[x][d]] + φ_[nei_[x][d_+d]];
+      return γ;
+    }
+    double gamma_pos_(const int& x) const
+    {
+      double γ = 0.;
+      for (int d = 0; d < d_; d ++) γ += φ_[nei_[x][d]];
+      return γ;
+    }
+
   public:
-    Field(const vector<int>& L, const double& am) : d_(L.size()), L_total_(1), L_(d_), am_(am), κ_(pow(2.*d_ + am_*am_, -.5)), data_(0)
+    Field(const vector<int>& L, const double& m) : d_(L.size()), L_total_(1), L_(d_), m_(m), φ_(0), nei_(0)
     {
       L_ = L;
       for (int d = 0; d < d_; d ++) L_total_ *= L_[d];
-      data_.resize(L_total_);
-      for (int x = 0; x < L_total_; x ++) data_[x] = 0.;
+      φ_.resize(L_total_, 0.);
+
+      nei_.resize(L_total_, vector<int>(2*d_));
+      vector<int> coord(d_, 0);
+      for (int x = 0; x < L_total_; x ++)
+      {
+        for (int d = 0; d < d_; d ++)
+        {
+          coord[d] = (coord[d]+1) % L_[d];
+          nei_[x][d] = index_(coord);
+          coord[d] = (coord[d]-2+L_[d]) % L_[d];
+          nei_[x][d_+d] = index_(coord);
+          coord[d] = (coord[d]+1) % L_[d];
+        }
+
+        coord.front() += 1;
+        for (int d = 0; d < d_-1; d ++)
+        {
+          if (coord[d] == L_[d])
+          {
+            coord[d] = 0;
+            coord[d+1] += 1;
+          }
+          else break;
+        }
+      }
     }
 
-    double& operator() (const int& x)       { return data_[x]; }
-    double  operator() (const int& x) const { return data_[x]; }
+    double& operator() (const int& x)       { return φ_[x]; }
+    double  operator() (const int& x) const { return φ_[x]; }
+    double& m()       { return m_; }
+    double  m() const { return m_; }
     int d() const { return d_; }
     int L_total() const { return L_total_; }
-    int L(const int& d) const { return L_[d]; }
-    double kappa() const { return κ_; }
-    double gamma(vector<int>& coord) const
+    vector<int> L()             const { return L_;    }
+    int         L(const int& d) const { return L_[d]; }
+    double kappa() const { return pow(2.*d_ + m_*m_, -.5); }
+
+    double act() const
     {
-      double γ = 0.;
-      for (int i = 0; i < d_; i ++)
+      double S = 0., fac = d_ + m_*m_/2.;
+      for (int x = 0; x < L_total_; x ++) S += φ_[x] * (fac*φ_[x] - gamma_pos_(x));
+      return S;
+    }
+    vector<double> Phi() const
+    {
+      int L_x_ = L_total_ / L_.back();
+      vector<double> Φ(L_.back(), 0.);
+      for (int t = 0; t < L_.back(); t ++)
       {
-        coord[i] = (coord[i]+1) % L_[i];
-        γ += data_[index_(coord)];
-        coord[i] = (coord[i]-2+L_[i]) % L_[i];
-        γ += data_[index_(coord)];
-        coord[i] = (coord[i]+1) % L_[i];
+        for (int x = 0; x < L_x_; x ++) Φ[t] += φ_[t*L_x_ + x];
       }
-      return γ;
+      return Φ;
+    }
+
+    double MA_Sweep(double& S, const double& ε)
+    {
+      double α = 0;
+      double κ2 = 1. / (2.*d_ + m_*m_);
+      for (int x = 0; x < L_total_; x ++)
+      {
+        double curr = φ_[x];
+        double prop = curr + 2*ε*standard_uniform(r) - ε;
+        double κ2γ = κ2*gamma_(x);
+        double ΔS = (pow(prop - κ2γ, 2.) - pow(curr - κ2γ, 2.))/(2*κ2);
+        if (ΔS <= 0. || standard_uniform(r) < exp(-ΔS))
+        {
+          φ_[x] = prop;
+          S += ΔS;
+          α += 1;
+        }
+      }
+      return α/L_total_;
+    }
+    void GS_Sweep(double& S)
+    {
+      double κ = kappa();
+      double κ2 = κ*κ;
+      for (int x = 0; x < L_total_; x ++)
+      {
+        double κ2γ = κ2*gamma_(x);
+        double prop = κ*standard_normal(r) + κ2γ;
+        double curr = φ_[x];
+        φ_[x] = prop;
+        S += (pow(prop - κ2γ, 2.) - pow(curr - κ2γ, 2.))/(2*κ2);
+      }
     }
 };
 
-void mse_err(const vector<double>& vec, const double& mean, double& mse, double& err)
+
+vector<double> corr(const vector<double>& Φ)
 {
-  int i;
-  int N = vec.size();
-
-  double var = 0.;
-  for (i = 0; i < N; i ++) var += pow(vec[i] - mean, 2.);
-  var /= double(N-1);
-
-  double result = 0.;
-  for (i = 0; i < N; i ++) result += pow(vec[i] - mean, 4.);
-
-  mse = var / double(N);
-  err = pow(((result/double(N)) - pow(var, 2.)*double(N-3)/double(N-1)) / pow(double(N), 3.), .5);
+  vector<double> c(Φ.size()/2 + 1, 0.);
+  for (int δ = 0; δ <= Φ.size()/2; δ ++)
+  {
+    for (int t = 0; t < Φ.size(); t ++) c[δ] += Φ[t]*Φ[(t+δ) % Φ.size()];
+  }
+  return c;
+}
+double mag(const vector<double>& Φ)
+{
+  double M = 0.;
+  for (int t = 0; t < Φ.size(); t ++) M += Φ[t];
+  return M;
 }
 
-vector<int> coord_init(const int& length)
+double mean(const vector<double>& chain)
 {
-  vector<int> coord(length);
-  for (int d = 0; d < length; d ++) coord[d] = 0;
-  return coord;
+  double sum = 0.;
+  for (int n = 0; n < chain.size(); n ++) sum += chain[n];
+  return sum/chain.size();
+}
+double quad_sum(const vector<double>& chain, const double& μ)
+{
+  double sum = 0.;
+  for (int n = 0; n < chain.size(); n ++) sum += pow(chain[n] - μ, 2.);
+  return sum;
+}
+double mean_error(const vector<double>& chain, const double& μ)
+{
+  return sqrt(quad_sum(chain, μ) / (chain.size()*(chain.size()-1.)));
 }
 
-void coord_update(vector<int>& coord, const Field& φ)
+void autocorr(const vector<double>& chain, const double& μ, double& τ, double& Δτ)
 {
-  coord[0] += 1;
-    for (int d = 0; d < φ.d()-1; d ++)
-    {
-      if (coord[d] == φ.L(d))
-      {
-        coord[d] = 0;
-        coord[d+1] += 1;
-      }
-      else break;
-    }
-}
-
-double normal_sample(const double& μ, const double& σ)
-{
-  auto norm = normal_distribution<double>{μ, σ};
-  return norm(r);
-}
-
-
-void GibbsCorrelation(Field& φ, const int& N, const int& disc, vector<vector<double>>& c_means, vector<vector<vector<double>>>& c_mses, vector<vector<vector<double>>>& c_mse_errs)
-{
-  for (int n = 0; n < disc; n ++)
+  int N = chain.size(), M = 0;
+  double fac = 2.*N/quad_sum(chain, μ);
+  τ = 1.;
+  do
   {
-    vector<int> coord = coord_init(φ.d());
-    for (int x = 0; x < φ.L_total(); x ++)
-    {
-      φ(x) = normal_sample(φ.kappa()*φ.kappa()*φ.gamma(coord), φ.kappa());
-      coord_update(coord, φ);
-    }
-    cout << n << "\n";
-  }
-
-  vector<vector<double>> Φs(φ.d());
-  for (int d = 0; d < φ.d(); d ++)
-  {
-    Φs[d] = vector<double>(φ.L(d));
-    for (int δ = 0; δ <= φ.L(d)/2; δ ++) c_means[d][δ] = 0.;
-  }
-
-  vector<vector<vector<double>>> cs(N);
-  for (int n = 0; n < N; n ++)
-  {
-    for (int d = 0; d < φ.d(); d ++)
-    {
-      for (int x = 0; x < φ.L(d); x ++) Φs[d][x] = 0.;
-    }
-
-    vector<int> coord = coord_init(φ.d());
-    for (int x = 0; x < φ.L_total(); x ++)
-    {
-      double site = normal_sample(φ.kappa()*φ.kappa()*φ.gamma(coord), φ.kappa());
-      φ(x) = site;
-      for (int d = 0; d < φ.d(); d ++) Φs[d][coord[d]] += site;
-      coord_update(coord, φ);
-    }
-
-    cs[n] = vector<vector<double>>(φ.d());
-    for (int d = 0; d < φ.d(); d ++)
-    {
-      cs[n][d] = vector<double>(φ.L(d)/2 + 1);
-      for (int δ = 0; δ <= φ.L(d)/2; δ ++)
-      {
-        cs[n][d][δ] = 0.;
-        for (int x = 0; x < φ.L(d); x ++) cs[n][d][δ] += Φs[d][x] * Φs[d][(x+δ) % φ.L(d)];
-        c_means[d][δ] += cs[n][d][δ];
-      }
-    }
-    cout << n << "\n";
-  }
-
-  int K = int(.5 + log2(double(N)));
-  for (int d = 0; d < φ.d(); d ++)
-  {
-    for (int δ = 0; δ <= φ.L(d)/2; δ ++)
-    {
-      c_means[d][δ] /= double(N);
-
-      vector<double> c(N);
-      for (int n = 0; n < N; n ++) c[n] = cs[n][d][δ];
-      int M = N;
-      for (int k = 0; k < K; k ++)
-      {
-        mse_err(c, c_means[d][δ], c_mses[d][δ][k], c_mse_errs[d][δ][k]);
-        M /= 2;
-        for (int n = 0; n < M; n ++) c[n] = .5 * (c[2*n] + c[2*n + 1]);
-        c.resize(M);
-      }
-    }
-  }
-}
-
-void MetropolisCorrelation(Field& φ, const double& ε, const int& N, const int& disc, vector<vector<double>>& c_means, vector<vector<vector<double>>>& c_mses, vector<vector<vector<double>>>& c_mse_errs)
-{
-  auto dist = uniform_real_distribution<double>{-ε, ε};
-  auto stand = uniform_real_distribution<double>{0., 1.};
-  for (int n = 0; n < disc; n ++)
-  {
-    vector<int> coord = coord_init(φ.d());
-    for (int x = 0; x < φ.L_total(); x ++)
-    {
-      double curr = φ(x);
-      double prop = curr + dist(r);
-      double κ2γ = φ.kappa() * φ.kappa() * φ.gamma(coord);
-      double diff = pow(curr - κ2γ, 2.) - pow(prop - κ2γ, 2.);
-      if (diff >= 0. || stand(r) < exp(diff / (2.*φ.kappa()*φ.kappa()))) φ(x) = prop;
-      coord_update(coord, φ);
-    }
-    cout << n << "\n";
-  }
-
-  vector<vector<double>> Φs(φ.d());
-  for (int d = 0; d < φ.d(); d ++)
-  {
-    Φs[d] = vector<double>(φ.L(d));
-    for (int δ = 0; δ <= φ.L(d)/2; δ ++) c_means[d][δ] = 0.;
-  }
-  vector<vector<vector<double>>> cs(N);
-
-  for (int n = 0; n < N; n ++)
-  {
-    for (int d = 0; d < φ.d(); d ++)
-    {
-      for (int x = 0; x < φ.L(d); x ++) Φs[d][x] = 0.;
-    }
-
-    vector<int> coord = coord_init(φ.d());
-    for (int x = 0; x < φ.L_total(); x ++)
-    {
-      double curr = φ(x);
-      double prop = curr + dist(r);
-      double κ2γ = φ.kappa() * φ.kappa() * φ.gamma(coord);
-      double diff = pow(curr - κ2γ, 2.) - pow(prop - κ2γ, 2.);
-      if (diff >= 0. || stand(r) < exp(diff / (2.*φ.kappa()*φ.kappa())))
-      {
-        φ(x) = prop;
-        for (int d = 0; d < φ.d(); d ++) Φs[d][coord[d]] += prop;
-      }
-      else
-      {
-        for (int d = 0; d < φ.d(); d ++) Φs[d][coord[d]] += curr;
-      }
-      coord_update(coord, φ);
-    }
-
-    cs[n] = vector<vector<double>>(φ.d());
-    for (int d = 0; d < φ.d(); d ++)
-    {
-      cs[n][d] = vector<double>(φ.L(d)/2 + 1);
-      for (int δ = 0; δ <= φ.L(d)/2; δ ++)
-      {
-        cs[n][d][δ] = 0.;
-        for (int x = 0; x < φ.L(d); x ++) cs[n][d][δ] += Φs[d][x] * Φs[d][(x+δ) % φ.L(d)];
-        c_means[d][δ] += cs[n][d][δ];
-      }
-    }
-    cout << n << "\n";
-  }
-
-  int K = int(.5 + log2(double(N)));
-  for (int d = 0; d < φ.d(); d ++)
-  {
-    for (int δ = 0; δ <= φ.L(d)/2; δ ++)
-    {
-      c_means[d][δ] /= double(N);
-
-      vector<double> c(N);
-      for (int n = 0; n < N; n ++) c[n] = cs[n][d][δ];
-      int M = N;
-      for (int k = 0; k < K; k ++)
-      {
-        mse_err(c, c_means[d][δ], c_mses[d][δ][k], c_mse_errs[d][δ][k]);
-        M /= 2;
-        for (int n = 0; n < M; n ++) c[n] = .5 * (c[2*n] + c[2*n + 1]);
-        c.resize(M);
-      }
-    }
-  }
-}
-
-void Correlation(Field& φ, const double& ε, const int& N, const int& disc, vector<vector<double>>& c_means, vector<vector<vector<double>>>& c_mses, vector<vector<vector<double>>>& c_mse_errs)
-{
-  if (ε < 0) GibbsCorrelation(φ, N, disc, c_means, c_mses, c_mse_errs);
-  else MetropolisCorrelation(φ, ε, N, disc, c_means, c_mses, c_mse_errs);
+    M++;
+    double sum = 0.;
+    for (int n = 0; n < N-M; n ++) sum += (chain[n]-μ)*(chain[n+M]-μ);
+    τ += sum/(N-M)*fac;
+  } while (M < 5*τ);
+  Δτ = τ*sqrt((4.*M+2.)/N);
 }
--- Sigma Model/Sigma Model.cpp
+++ Sigma Model/Sigma Model.cpp
@@ -4,17 +4,12 @@
 #include <valarray>
 #include <cmath>
 #include <random>
-#include <complex>
-#include <chrono>
 using namespace std;
 
-auto r = mt19937 {123};
-auto n_dist = normal_distribution<double>{0, 1./sqrt(2.)};
-auto e_dist = exponential_distribution<double>{1.};
-auto u_dist = uniform_real_distribution<double>{0., 1.};
-auto n1_dist = uniform_real_distribution<double>{-1., 1.};
-static const double π = 3.14159265358979323846264338327950;
-auto θ_dist = uniform_real_distribution<double>{0., 2.*π};
+mt19937 r(1234);
+uniform_real_distribution<double> standard_uniform(0., 1.);
+normal_distribution<double> standard_normal(0., 1.);
+exponential_distribution<double> standard_exponential(1.);
 
 
 class SU2
@@ -22,8 +17,8 @@ class SU2
   private:
     valarray<double> u_;
   public:
-    SU2(const valarray<double>& u) : u_(u) {}
-    double  operator[] (const int& i) const { return u_[i]; }
+    SU2(const valarray<double>& u) : u_(u) { }
+    double operator[] (const int& i) const { return u_[i]; }
     valarray<double> comp() const { return u_; }
     SU2 operator* (const SU2& v) const { return SU2({
       u_[0]*v[0] - u_[1]*v[1] - u_[2]*v[2] - u_[3]*v[3],
@@ -35,279 +30,177 @@ class SU2
 class Field
 {
   private:
-    int d_, L_total_, T_, Δ_;
+    int d_, L_total_;
     vector<int> L_;
     double β_, λ_;
-    vector<SU2> data_;
+    vector<SU2> φ_;
+    vector<vector<int>> nei_;
+
     int index_(const vector<int>& coord) const
     {
-      int index = coord[0], Π = L_[0];
-      for (int i = 1; i < d_; i ++)
+      int index = coord.front(), Π = L_.front();
+      for (int d = 1; d < d_; d ++)
       {
-        index += coord[i] * Π;
-        Π *= L_[i];
+        index += coord[d] * Π;
+        Π *= L_[d];
       }
       return index;
     }
-  public:
-    Field(const vector<int>& L, const double& β, const double& λ) : d_(L.size()), L_total_(1), T_(L.back()), Δ_(T_/2 + 1), L_(L), β_(β), λ_(λ)
-    {
-      for (int d = 0; d < d_; d ++) L_total_ *= L_[d];
-      for (int x = 0; x < L_total_; x ++) data_.push_back(SU2({ 1., 0., 0., 0. }));
-    }
 
-    SU2& operator() (const int& x)       { return data_[x]; }
-    SU2  operator() (const int& x) const { return data_[x]; }
-    int d() const { return d_; }
-    int L_total() const { return L_total_; }
-    int T() const { return T_; }
-    int Delta() const { return Δ_; }
-    int L(const int& d) const { return L_[d]; }
-    double beta() const { return β_; }
-    valarray<double> Sigma(vector<int>& coord) const
+    valarray<double> Sigma_(const int& x) const
     {
       valarray<double> Σ = { λ_, 0., 0., 0. };
-      for (int i = 0; i < d_; i ++)
-      {
-        coord[i] = (coord[i]+1) % L_[i];
-        Σ += data_[index_(coord)].comp();
-        coord[i] = (coord[i]-2+L_[i]) % L_[i];
-        Σ += data_[index_(coord)].comp();
-        coord[i] = (coord[i]+1) % L_[i];
-      }
+      for (int d = 0; d < d_; d ++) Σ += φ_[nei_[x][d]].comp() + φ_[nei_[x][d_+d]].comp();
       return Σ;
+    }
+    valarray<double> Sigma_pos_(const int& x) const
+    {
+      valarray<double> Σ = { λ_, 0., 0., 0. };
+      for (int d = 0; d < d_; d ++) Σ += φ_[nei_[x][d]].comp();
+      return Σ;
+    }
+
+  public:
+    Field(const vector<int>& L, const double& β, const double& λ) : d_(L.size()), L_total_(1), L_(L), β_(β), λ_(λ)
+    {
+      for (int d = 0; d < d_; d ++) L_total_ *= L_[d];
+      φ_.resize(L_total_, SU2({ 1., 0., 0., 0. }));
+
+      nei_.resize(L_total_, vector<int>(2*d_));
+      vector<int> coord(d_, 0);
+      for (int x = 0; x < L_total_; x ++)
+      {
+        for (int d = 0; d < d_; d ++)
+        {
+          coord[d] = (coord[d]+1) % L_[d];
+          nei_[x][d] = index_(coord);
+          coord[d] = (coord[d]-2+L_[d]) % L_[d];
+          nei_[x][d_+d] = index_(coord);
+          coord[d] = (coord[d]+1) % L_[d];
+        }
+
+        coord.front() += 1;
+        for (int d = 0; d < d_-1; d ++)
+        {
+          if (coord[d] == L_[d])
+          {
+            coord[d] = 0;
+            coord[d+1] += 1;
+          }
+          else break;
+        }
+      }
+    }
+
+    SU2& operator() (const int& x)       { return φ_[x]; }
+    SU2  operator() (const int& x) const { return φ_[x]; }
+    double& beta()       { return β_; }
+    double  beta() const { return β_; }
+    double& lambda()       { return λ_; }
+    double  lambda() const { return λ_; }
+    int d() const { return d_; }
+    int L_total() const { return L_total_; }
+    vector<int> L()             const { return L_;    }
+    int         L(const int& d) const { return L_[d]; }
+
+    double act() const
+    {
+      double S = 0.;
+      for (int x = 0; x < L_total_; x ++) S -= β_*((Sigma_pos_(x)*φ_[x].comp()).sum());
+      return S;
+    }
+    vector<valarray<double>> Phi() const
+    {
+      int L_x_ = L_total_ / L_.back();
+      vector<valarray<double>> Φ(L_.back(), valarray<double>(0., 4));
+      for (int t = 0; t < L_.back(); t ++)
+      {
+        for (int x = 0; x < L_x_; x ++) Φ[t] += φ_[t*L_x_ + x].comp();
+      }
+      return Φ;
+    }
+
+    int GS_Sweep(double& S)
+    {
+      int gen = 0;
+      for (int x = 0; x < L_total_; x ++)
+      {
+        valarray<double> Σ = Sigma_(x);
+        double sqrtdet = sqrt((Σ*Σ).sum());
+
+        valarray<double> v(4);
+        double U;
+        double ρ = β_*sqrtdet;
+        do
+        {
+          double A = standard_normal(r) / sqrt(2.);
+          U = standard_uniform(r);
+          v[0] = 1. - (A*A + standard_exponential(r)) / ρ;
+          gen += 1;
+        } while (2*U*U > v[0] + 1.);
+
+        v[1] = 2.*standard_uniform(r) - 1.;
+        double θ = 2.*M_PI*standard_uniform(r);
+        double fac = sqrt(1. - v[1]*v[1]);
+        v[2] = cos(θ)*fac;
+        v[3] = sin(θ)*fac;
+
+        fac = sqrt(1. - v[0]*v[0]);
+        for (int i = 1; i < 4; i ++) v[i] *= fac;
+
+        valarray<double> curr = φ_[x].comp();
+        φ_[x] = SU2(v) * SU2(Σ / sqrtdet);
+        S -= β_*((Σ*(φ_[x].comp() - curr)).sum());
+      }
+      return gen;
     }
 };
 
 
-vector<int> coord_init(const int& length)
+vector<valarray<double>> corr(const vector<valarray<double>>& Φ)
 {
-  vector<int> coord(length);
-  for (int d = 0; d < length; d ++) coord[d] = 0;
-  return coord;
+  vector<valarray<double>> c(Φ.size()/2 + 1, valarray<double>(0., 4));
+  for (int δ = 0; δ <= Φ.size()/2; δ ++)
+  {
+    for (int t = 0; t < Φ.size(); t ++) c[δ] += Φ[t]*Φ[(t+δ) % Φ.size()];
+  }
+  return c;
+}
+valarray<double> mag(const vector<valarray<double>>& Φ)
+{
+  valarray<double> M(0., 4);
+  for (int t = 0; t < Φ.size(); t ++) M += Φ[t];
+  return M;
 }
 
-void coord_update(vector<int>& coord, const Field& φ)
+double mean(const vector<double>& chain)
 {
-  coord[0] += 1;
-    for (int d = 0; d < φ.d()-1; d ++)
-    {
-      if (coord[d] == φ.L(d))
-      {
-        coord[d] = 0;
-        coord[d+1] += 1;
-      }
-      else break;
-    }
+  double sum = 0.;
+  for (int n = 0; n < chain.size(); n ++) sum += chain[n];
+  return sum/chain.size();
+}
+double quad_sum(const vector<double>& chain, const double& μ)
+{
+  double sum = 0.;
+  for (int n = 0; n < chain.size(); n ++) sum += pow(chain[n] - μ, 2.);
+  return sum;
+}
+double mean_error(const vector<double>& chain, const double& μ)
+{
+  return sqrt(quad_sum(chain, μ) / (chain.size()*(chain.size()-1.)));
 }
 
-void mse_err(const vector<double>& vec, const double& mean, double& mse, double& err)
+void autocorr(const vector<double>& chain, const double& μ, double& τ, double& Δτ)
 {
-  int N = vec.size();
-
-  double var = 0.;
-  for (int i = 0; i < N; i ++) var += pow(vec[i] - mean, 2.);
-  var /= double(N-1);
-
-  double result = 0.;
-  for (int i = 0; i < N; i ++) result += pow(vec[i] - mean, 4.);
-
-  mse = var / double(N);
-  err = pow(((result/double(N)) - pow(var, 2.)*double(N-3)/double(N-1)) / pow(double(N), 3.), .5);
-}
-
-void mse_err(const vector<int>& vec, const double& mean, double& mse, double& err)
-{
-  int N = vec.size();
-
-  double var = 0.;
-  for (int i = 0; i < N; i ++) var += pow(vec[i] - mean, 2.);
-  var /= double(N-1);
-
-  double result = 0.;
-  for (int i = 0; i < N; i ++) result += pow(vec[i] - mean, 4.);
-
-  mse = var / double(N);
-  err = pow(((result/double(N)) - pow(var, 2.)*double(N-3)/double(N-1)) / pow(double(N), 3.), .5);
-}
-
-
-double random_v0(const double& rho, int& rejections)
-{
-  double A, U, v0;
-  rejections --;
+  int N = chain.size(), M = 0;
+  double fac = 2.*N/quad_sum(chain, μ);
+  τ = 1.;
   do
   {
-    A = n_dist(r);
-    U = u_dist(r);
-    v0 = 1. - (A*A + e_dist(r)) / rho;
-    rejections ++;
-  }
-  while (2*U*U > v0 + 1.);
-  return v0;
-}
-
-SU2 random_SU2(const double& ρ, int& rejections)
-{
-  valarray<double> v(4);
-  v[0] = random_v0(ρ, rejections);
-  v[1] = n1_dist(r);
-  {
-    double θ = θ_dist(r);
-    v[2] = cos(θ);
-    v[3] = sin(θ);
-  }
-  {
-    double sqrt = pow(1. - v[1]*v[1], .5);
-    for (int i = 2; i < 4; i ++) v[i] *= sqrt;
-    sqrt = pow(1. - v[0]*v[0], .5);
-    for (int i = 1; i < 4; i ++) v[i] *= sqrt;
-  }
-  return SU2(v);
-}
-
-SU2 GibbsStep(Field& σ, vector<int>& coord, int& rejections)
-{
-  valarray<double> Σ = σ.Sigma(coord);
-  double sqrtdet = 0.;
-  for (int i = 0; i < 4; i ++) sqrtdet += Σ[i] * Σ[i];
-  sqrtdet = pow(sqrtdet, .5);
-  return random_SU2(σ.beta() * sqrtdet, rejections) * SU2(Σ / sqrtdet);
-}
-
-void GibbsCorrelation(Field& σ, const int& N, const int& disc, vector<vector<double>>& c_means, vector<vector<vector<double>>>& c_mses, vector<vector<vector<double>>>& c_mse_errs, double& rej_mean, vector<double>& rej_mses, vector<double>& rej_mse_errs, vector<vector<double>>& am_means, vector<vector<double>>& am_mses)
-{
-  auto start = chrono::steady_clock::now();
-  vector<int> coord;
-  int disc_rej = 0;
-  for (int n = 0; n < disc; n ++)
-  {
-    coord = coord_init(σ.d());
-    for (int x = 0; x < σ.L_total(); x ++)
-    {
-      σ(x) = GibbsStep(σ, coord, disc_rej);
-      coord_update(coord, σ);
-    }
-  }
-  chrono::duration<double> time = chrono::steady_clock::now() - start;
-  cout << "Discarding: " << time.count() << " seconds\n";
-
-  start = chrono::steady_clock::now();
-  vector<vector<double>> Φs(σ.T());
-  for (int t = 0; t < σ.T(); t ++) Φs[t] = vector<double>(4);
-  for (int i = 0; i < 5; i ++)
-  {
-    for (int δ = 0; δ < σ.Delta(); δ ++) c_means[i][δ] = 0.;
-  }
-  rej_mean = 0.;
-  vector<vector<vector<double>>> cs(N);
-  vector<int> rs(N);
-
-  for (int n = 0; n < N; n ++)
-  {
-    for (int t = 0; t < σ.T(); t ++)
-    {
-      for (int i = 0; i < 4; i ++) Φs[t][i] = 0.;
-    }
-
-    coord = coord_init(σ.d());
-    rs[n] = 0;
-    for (int x = 0; x < σ.L_total(); x ++)
-    {
-      σ(x) = GibbsStep(σ, coord, rs[n]);
-      for (int i = 0; i < 4; i ++) Φs[coord.back()][i] += σ(x)[i];
-      coord_update(coord, σ);
-    }
-
-    cs[n] = vector<vector<double>>(5);
-    for (int i = 0; i < 5; i ++)
-    {
-      cs[n][i] = vector<double>(σ.Delta());
-      for (int δ = 0; δ < σ.Delta(); δ ++) cs[n][i][δ] = 0.;
-    }
-
-    for (int δ = 0; δ < σ.Delta(); δ ++)
-    {
-      for (int i = 0; i < 4; i ++)
-      {
-        for (int t = 0; t < σ.T(); t ++) cs[n][i][δ] += Φs[t][i] * Φs[(t+δ) % σ.T()][i];
-      }
-      for (int i = 1; i < 4; i ++) cs[n][4][δ] += cs[n][i][δ];
-      cs[n][4][δ] /= 3.;
-      for (int i = 0; i < 5; i ++) c_means[i][δ] += cs[n][i][δ];
-    }
-    rej_mean += rs[n];
-  }
-  time = chrono::steady_clock::now() - start;
-  cout << "Simulating: " << time.count() << " seconds\n";
-
-  start = chrono::steady_clock::now();
-  int K = int(.5 + log2(double(N)));
-  for (int i = 0; i < 5; i ++)
-  {
-    for (int δ = 0; δ < σ.Delta(); δ ++)
-    {
-      c_means[i][δ] /= N;
-      vector<double> c(N);
-      for (int n = 0; n < N; n ++) c[n] = cs[n][i][δ];
-      int M = N;
-      for (int k = 0; k < K; k ++)
-      {
-        mse_err(c, c_means[i][δ], c_mses[i][δ][k], c_mse_errs[i][δ][k]);
-        M /= 2;
-        for (int n = 0; n < M; n ++) c[n] = .5 * (c[2*n] + c[2*n + 1]);
-        c.resize(M);
-      }
-    }
-    for (int n = 0; n < N; n ++) cs[n][i].resize(3);
-  }
-  time = chrono::steady_clock::now() - start;
-  cout << "Correlation binning: " << time.count() << " seconds\n";
-
-  start = chrono::steady_clock::now();
-  rej_mean /= N;
-  int M = N;
-  for (int k = 0; k < K; k ++)
-  {
-    mse_err(rs, rej_mean, rej_mses[k], rej_mse_errs[k]);
-    M /= 2;
-    for (int n = 0; n < M; n ++) rs[n] = .5 * (rs[2*n] + rs[2*n + 1]);
-    rs.resize(M);
-  }
-  time = chrono::steady_clock::now() - start;
-  cout << "Rejection binning: " << time.count() << " seconds\n";
-
-  start = chrono::steady_clock::now();
-  M = N;
-  for (int k = 0; k < K; k ++)
-  {
-    vector<double> cj(3);
-    vector<double> amj(M);
-    for (int i = 0; i < 5; i ++)
-    {
-      am_means[i][k] = 0.;
-      for (int n = 0; n < M; n ++)
-      {
-        for (int δ = 0; δ <= 2; δ ++) cj[δ] = ((M*c_means[i][δ]) - cs[n][i][δ]) / (M - 1.);
-        amj[n] = log((cj[0] - cj[2] + sqrt(pow(cj[0] - cj[2], 2.) - 4.*(cj[1]-cj[2])*(cj[0]-cj[1]))) / (2.*(cj[1] - cj[2])));
-        am_means[i][k] += amj[n];
-      }
-      am_means[i][k] /= M;
-
-      am_mses[i][k] = 0.;
-      for (int n = 0; n < M; n ++) am_mses[i][k] += pow(am_means[i][k] - amj[n], 2.);
-      am_mses[i][k] *= (M - 1.) / double(M);
-    }
-    M /= 2;
-    for (int n = 0; n < M; n ++)
-    {
-      for (int i = 0; i < 5; i ++)
-      {
-        for (int δ = 0; δ <= 2; δ ++) cs[n][i][δ] = .5 * (cs[2*n][i][δ] + cs[2*n + 1][i][δ]);
-      }
-    }
-    cs.resize(M);
-  }
-  time = chrono::steady_clock::now() - start;
-  cout << "Mass jackknifing: " << time.count() << " seconds\n";
+    M++;
+    double sum = 0.;
+    for (int n = 0; n < N-M; n ++) sum += (chain[n]-μ)*(chain[n+M]-μ);
+    τ += sum/(N-M)*fac;
+  } while (M < 5*τ);
+  Δτ = τ*sqrt((4.*M+2.)/N);
 }
